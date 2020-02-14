0457d96435ffe7989b11207a19e11c020ad299a3
Martin Jérémy <jeremy.martin12@hotmail.fr>
2020-02-14 09:56:02 +0100

    Update Perceptron.py

--- Week2/Perceptron.py
+++ Week2/Perceptron.py
@@ -6,31 +6,31 @@ Created on Thu Feb 13 20:39:43 2020
 """
 import numpy as np
 
-x = np.array([[1,0],[-1,1.5],[-1,-1]])
-y = np.array([1,-1,1])
+x = np.array([[-4,2],[-2,1],[-1,-1],[2,2],[1,-2]])
+y = np.array([1,1,-1,-1,-1])
 
 def Perceptron(x,y,T):
-    theta = np.array([0,0])
-    theta_0 = 0
+    counter = []
+    theta = np.array([-3,3])
+    theta_0 = -3
     for t in range(1,T):
-        for i in range(0,len(y)-1):
+        for i in range(0,len(y)):
             if y[i]*(np.dot(theta,x[i])+theta_0) <= 0:
                 theta = theta + y[i]*x[i]
                 theta_0 = theta_0 + y[i]
+                counter.append(theta)
+    return theta, theta_0, len(counter)
                 
-    return theta, theta_0
+    #return theta, theta_0, counter
 
 def PerceptronOrigin(x,y,T):
-    counter = 0
+    counter = []
     theta = np.array([0,0])
     for t in range(1,T):
-        tmp = theta
         for i in range(0,len(y)):
             if y[i]*(np.dot(theta,x[i])) <= 0:
                 theta = theta + y[i]*x[i]
-                print(theta)
-        if tmp is not theta:
-            counter=counter+1
-    return theta, counter
+                counter.append(theta)
+    return counter, len(counter)
 
-print(PerceptronOrigin(x,y,5))
+print(Perceptron(x,y,10))
